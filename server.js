9a8ebca861582503c3f6c830d26e2bc260a0618a
Liza <liza0725.be23@chitkara.edu.in>
2025-04-16 13:19:10 +0530

    env changes

--- server.js
+++ server.js
@@ -2,31 +2,40 @@ const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const path = require('path');
+const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
 
+dotenv.config();
+
 const app = express();
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://LizaJulaha:Liza1234@cluster0.rkzcks7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+  .catch(err => console.error("MongoDB connection error:", err));
 
-// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'secretkey', resave: false, saveUninitialized: false, cookie: { secure: false } }));
 
-// EJS Setup
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'fallbacksecret',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false } 
+}));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'firstpage.html'));
 });
-// Routes
+
 app.use('/', authRoutes);
 
-// Server
-app.listen(3000, () => console.log('Server started on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
