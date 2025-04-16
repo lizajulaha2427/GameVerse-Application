f677b09713791b5adb1653e7ae3d82b091ce02f4
Liza <156740104+lizajulaha2427@users.noreply.github.com>
2025-04-16 16:43:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ app.use('/', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
